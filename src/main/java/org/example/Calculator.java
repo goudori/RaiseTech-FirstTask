5f0f1effb0ffc8616a2bedb2654546c9e39fcaf9
gobird <monaka@example.com>
2025-02-13 21:00:42 +0900

    first-commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * 問題1:Javaの基本構文を使用して、計算機プログラムを作成してください。加減乗除の四則演算ができ、 ユーザーから入力を受け取り、結果を表示するものとします。
+ */
+public class Calculator {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    boolean isContinue = true; //計算を続ける
+
+    while (isContinue) {
+
+      try {
+
+        System.out.println("最初の数値を入力して下さい。");
+
+        int firstNum = scanner.nextInt(); //最初の数値を入力
+
+        System.out.println("(+,-,*,/)の中から演算子を入力して下さい");
+        char operator = scanner.next().charAt(0);
+
+        System.out.println("次の数値を入力して下さい。");
+
+        int secondNum = scanner.nextInt(); //次の数値を入力
+
+        int result = 0;
+
+        switch (operator) {
+          case '+': //足し算
+            result = firstNum + secondNum;
+            break;
+
+          case '-': //引き算
+            result = firstNum - secondNum;
+            break;
+
+          case '*': // 掛け算
+            result = firstNum * secondNum;
+            break;
+
+          case '/': // 割り算
+            result = firstNum / secondNum;
+            break;
+
+          default:
+            System.out.println("不正な数値が入力されたので、正しい数値を入力して下さい。");
+            System.exit(1);
+
+
+        }
+
+        System.out.println("計算結果=" + result + "です。");
+      } catch (InputMismatchException e) {
+        System.out.println("不正な入力が行われました。");
+        scanner.next();
+
+      } catch (ArithmeticException e) {
+        System.out.println("0で割る事はできません。");
+
+      } catch (Exception e) {
+        System.out.println("不明なエラー発生!!!");
+
+      }
+      System.out.println("計算を続けるなら、「y」、終了するなら、「n」を入力する。");
+
+      char response = scanner.next().charAt(0);
+
+      if (response == 'n' || response == 'N') {
+        isContinue = false; //計算を終了する(while文を抜ける)
+      }
+    }
+  }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.io.File;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * 「Windowsの場合は、Shift + F10で実行して下さい。」,「Macの場合は、Control + R で実行して下さい。」
+ * <p>
+ * コンソール画面で、「calc,student.phone,exit」の中から選択して実行して下さい。
+ * <p>
+ * ※もしMain.javaファイルで実行できない場合は、以下のファイルの中から選択して、実行して下さい。
+ * <p>
+ * Calculator.java →計算機プログラム Students.java　→学生管理プログラム  PhoneRegex.java →携帯電話番号チェックプログラム
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      try {
+        System.out.println("calc, student,phone,exitの中から選択して実行してください。");
+
+        String input = scanner.next();
+
+        switch (input) {
+
+          case "calc": // Calculator.java
+            Calculator.main(args);
+            break;
+
+          case "student": // Students.java
+            Students.main(args);
+            break;
+
+          case "phone": // PhoneRegex.java
+            PhoneRegex.main(args);
+            break;
+
+          case "exit": // 終了
+            System.out.println("終了します。");
+            System.exit(0);
+
+          default:
+            System.out.println("不明なコマンドです。");
+        }
+      } catch (InputMismatchException i) {
+        System.out.println("不明なコマンドです。");
+      } catch (Exception e) {
+        System.out.println("不明なコマンドです。");
+      }
+    }
+
+
+  }
+
+
+}
